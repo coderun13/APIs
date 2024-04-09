4543cc5ab73dc422241b4a2031defd725ee4c3e2
coderun13 <snehalsingh.0407@gmail.com>
2024-04-09 17:33:29 +0530

    new files added

--- REST/index.js
+++ REST/index.js
@@ -4,12 +4,14 @@ const port = 3030;
 const path = require("path");
 const {v4: uuidv4 } = require('uuid');
 uuidv4(); // => '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const methodOverride = require('method-override')
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views")); // path for views folder
 
-app.use(express.static(path.join(__dirname,"public"))); // path for public folder
 
+app.use(express.static(path.join(__dirname,"public"))); // path for public folder
+app.use(methodOverride('_method'))
 
 app.use(express.urlencoded ({ extended : true })); //standard line //middleware
 app.use(express.json());//middleware
@@ -65,10 +67,24 @@ app.post("/posts", (req,res) => {
   // res.send("post requests working"); //submit page(confirmation)
 });
 
+app.patch("/posts/:id",(req,res)=> { //patch request
+    let {id} = req.params;
+    let newcontent = req.body.content;
+    let post = posts.find((p) => id == p.id);
+    post.content = newcontent;
+    console.log(post);
+    res.redirect("/posts");
+});
 
+app.get("/posts/:id/edit", (req,res) => { //create post page
+    let {id} = req.params;
+    let post = posts.find((p) => id == p.id);
+    res.render("edit.ejs",{post});
 
+});
 
-
-
-
-
+app.delete("/posts/:id",(req,res)=> { //delete request
+    let {id} = req.params;
+   posts = posts.filter((p) => id !== p.id);
+   res.redirect("/posts");
+});
